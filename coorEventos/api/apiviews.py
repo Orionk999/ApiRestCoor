bac3cac868d7c19cfad8a7a7db98efcf5233f48d
Orionk999 <ing.daniel.castillo11@gmail.com>
2024-02-21 23:38:16 -0500

    Developing Maps

--- coorEventos/api/apiviews.py
+++ coorEventos/api/apiviews.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
+import requests
 from django.contrib.auth import authenticate
 
 from .models import Evento
@@ -45,3 +46,25 @@ class LoginView(APIView):
         else:
             return Response({"error": "Credenciales Incorrectas"},
                             status=status.HTTP_400_BAD_REQUEST)
+class GetAddress(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        latitude = request.data.get("latitude")
+        longitude = request.data.get("longitude")
+        base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+        params = {
+            "latlng": f"{latitude},{longitude}",
+            "key": "YOURAPIKEYFROMGOOGLE",
+       }
+        try:
+            response = requests.get(base_url, params=params)
+            data = response.json()
+            if data["status"] == "OK":
+                formatted_address = data["results"]
+                return Response({"status": "OK", "formatted_address":formatted_address})
+            else:
+                print("Error en la solicitud:", data["status"])
+                return None
+        except Exception as e:
+            print("Error en la solicitud:", str(e))
+            return None
